ffeac1ea9d6d9ee021db24e0b79fcbaed782f3b0
austinbhan <austinbhan@outlook.com>
2022-05-19 15:34:48 -0700

    Made Trail Render

---
+++ other-page/render-item.js
@@ -0,0 +1,14 @@
+export function renderItem(item) {
+    const li = document.createElement('li');
+    li.setAttribute('id', `${item}-container`);
+
+    const p = document.createElement('p');
+    p.setAttribute('id', 'item');
+    p.textContent = `${item.item}`;
+
+    const number = document.createElement('p');
+    number.setAttribute('id', 'number');
+    number.textContent = `${item.quantity}`;
+
+    li.append(p, number);
+}
