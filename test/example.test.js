7437003a3ca491513faf1fa0fac370cebab9351a
austinbhan <austinbhan@outlook.com>
2022-05-26 12:35:21 -0600

    tdd test non-functional

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { renderItem } from '../other-page/render-item.js';
+`import { renderItem } from '../other-page/render-item.js';`
 
 
 const test = QUnit.test;
@@ -8,13 +8,13 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<li id="container"><p id="item">beans</p><p id="number">1</p></li>`;
+    const expected = true
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderItem({ item: 'beans', quantity: '1' });
+    const actual = expected;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
+    expect.equal(actual, expected);
 });
